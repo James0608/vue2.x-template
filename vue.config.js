4d413125479e1215ece02458424558850757e271
James0608 <529881691@qq.com>
2021-05-05 22:49:56 +0800

    add pages & fix error

--- vue.config.js
+++ vue.config.js
@@ -6,17 +6,36 @@ module.exports = {
   outputDir: 'dist',
   // 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录, 默认值为‘’
   assetsDir: '',
+  pages: {
+    index: {
+      // page 的入口
+      entry: 'src/main.js',
+      // 模板来源
+      template: 'public/index.html',
+      // 在 dist/index.html 的输出
+      filename: 'index.html',
+      // 当使用 title 选项时，
+      // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'vue2.x-template',
+    },
+  },
   // 是否在开发环境下通过 eslint-loader 在每次保存时 lint 代码。这个值会在 @vue/cli-plugin-eslint 被安装之后生效, 默认值为 'default'
   lintOnSave: 'default',
   // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建
   productionSourceMap: false,
   // details：https://cli.vuejs.org/zh/guide/webpack.html
-  configureWebpack: (config) => {
-    if (process.env.NODE_ENV === 'production') {
-      // 为生产环境修改配置...
-    } else {
-      // 为开发环境修改配置...
-    }
+  configureWebpack: {
+    // 所有 webpack-dev-server 的选项都支持
+    devServer: {
+      port: 8080,
+      proxy: {
+        '/api': {
+          target: '<url>',
+          ws: true,
+          changeOrigin: true,
+        },
+      },
+    },
   },
   // details：https://cli.vuejs.org/zh/guide/webpack.html
   chainWebpack: (config) => {
@@ -30,23 +49,12 @@ module.exports = {
     // extract: false,
     sourceMap: false,
     loaderOptions: {
-      css: {
-        // 这里的选项会传递给 css-loader
-      },
-      postcss: {
-        // 这里的选项会传递给 postcss-loader
-      },
-    },
-  },
-  // 所有 webpack-dev-server 的选项都支持
-  devServer: {
-    port: 8080,
-    proxy: {
-      '/api': {
-        target: '<url>',
-        ws: true,
-        changeOrigin: true,
-      },
+      // css: {
+      //   // 这里的选项会传递给 css-loader
+      // },
+      // postcss: {
+      //   // 这里的选项会传递给 postcss-loader
+      // },
     },
   },
   // 这是一个不进行任何 schema 验证的对象，因此它可以用来传递任何第三方插件选项。
